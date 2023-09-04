0348f6116c85bcc82444c62976605c1aba036984
phlirajr <phlj@discente.ifpe.edu.br>
2023-09-04 18:03:35 -0300

    tela de login pronta

--- screens/login.js
+++ screens/login.js
@@ -5,34 +5,39 @@ import { Avatar, Button, Input } from 'react-native-elements';
 
 function Login({navigation}) {
     return (
-        <View style={{flex: 1, flexDirection: "column", backgroundColor: "#FAF9F6"}}>
-          <View style={{alignItems:'center'}}>
-            <Avatar              
-              rounded
-              size ='xlarge'
-              icon={{name: 'user', type: 'font-awesome'}}
-            />
+        <View style={{flex: 1, flexDirection: "column", justifyContent:'center', alignItems: 'center', paddingTop: 200}}>
+          <View style={{alignItems: 'center'}}>
+          <Avatar
+            rounded
+            size = "xlarge"
+            source={{
+              uri:
+                'https://image.pngaaa.com/567/2189567-middle.png',
+            }}
+          />
              <Input
                 containerStyle={{alignItems: 'center', justifyContent: 'center'}}
                 placeholder="Nome"
                 leftIcon={{ type: 'font-awesome', name: 'user', color: '#d3d3d3'}}
                 onChangeText={value => this.setState({ comment: value })}
+                style = {{marginTop: 10, padding: 2}}
               />
              <Input
                 containerStyle={{alignItems: 'center', justifyContent: 'center'}}
                 placeholder="Senha"
                 leftIcon={{ type: 'font-awesome', name: 'lock', color: '#d3d3d3'}}
                 onChangeText={value => this.setState({ comment: value })}
+                style={{padding: 2}}
               />
           </View>
-          <View style={{flex:1, alignItems: 'center', gap: 10}}>
+          <View style={{flex:1, alignItems: 'center', gap: 20, paddingTop:20}}>
             <Button
               containerStyle={{width: 200}}
               title="Login"
               onPress={()=>navigation.navigate("Contatos")}                        
             />
             <Button
-              containerStyle={{width: 200, }}
+              containerStyle={{width: 200 }}
               title="Cadastre-se"
               type='outline'
               onPress={()=>navigation.navigate("Contatos")}          
